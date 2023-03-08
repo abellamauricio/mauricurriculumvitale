1f3a3c5cc2734b113af017ecb08d0755321ab332
abellamauricio <127263280+abellamauricio@users.noreply.github.com>
2023-03-07 23:30:08 -0300

    cv de mauricio

---
+++ java.js
@@ -0,0 +1,7 @@
+document.getElementById('boton').onclick = function() {
+
+console.log("capturamos el evento click");
+document.getElementById("demo").innerHTML ="Quiero agradecerle inmensamente, que se haya tomado unos minutos de su tiempo para analizar este curriculum vitae ";
+
+
+}
